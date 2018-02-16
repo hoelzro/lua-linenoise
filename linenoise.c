a93714937bd4632c1d2f65acd04262c8db07fd19
Rob Hoelz <rob@hoelz.ro>
2018-02-15 23:09:46 -0600

    Handle errors in completion callbacks
    Error handling in hints callbacks to follow.
    
    Handles GH #14 by propagating the error up the call stack and
    terminating linenoise() prematurely

--- linenoise.c
+++ linenoise.c
@@ -39,6 +39,7 @@
 static int completion_func_ref;
 static int hints_func_ref;
 static lua_State *completion_state;
+static int callback_error_ref;
 
 static int handle_ln_error(lua_State *L)
 {
@@ -55,6 +56,7 @@ static int handle_ln_ok(lua_State *L)
 static int completion_callback_wrapper(const char *line, linenoiseCompletions *completions)
 {
     lua_State *L = completion_state;
+    int status;
 
     lua_rawgeti(L, LUA_REGISTRYINDEX, completion_func_ref);
     *((linenoiseCompletions **) lua_newuserdata(L, sizeof(linenoiseCompletions *))) = completions;
@@ -63,10 +65,14 @@ static int completion_callback_wrapper(const char *line, linenoiseCompletions *c
 
     lua_pushstring(L, line);
 
-    // XXX handle error
-    lua_pcall(L, 2, 0, 0);
+    status = lua_pcall(L, 2, 0, 0);
 
-    return 0;
+    if(status == LUA_OK) {
+        return 0;
+    } else {
+        lua_rawseti(L, LUA_REGISTRYINDEX, callback_error_ref);
+        return 1;
+    }
 }
 
 static char *
@@ -135,11 +141,20 @@ static int l_linenoise(lua_State *L)
     char *line;
 
     completion_state = L;
+    lua_pushliteral(L, "");
+    lua_rawseti(L, LUA_REGISTRYINDEX, callback_error_ref);
     line = linenoise(prompt);
     completion_state = NULL;
 
     if(! line) {
         return handle_ln_error(L);
+    }
+    lua_rawgeti(L, LUA_REGISTRYINDEX, callback_error_ref);
+    if(strlen(lua_tostring(L, -1)) != 0) {
+        lua_pushnil(L);
+        lua_insert(L, -2);
+        linenoiseFree(line);
+        return 2;
     }
     lua_pushstring(L, line);
     linenoiseFree(line);
@@ -292,6 +307,9 @@ luaL_Reg linenoise_methods[] = {
 
 LN_EXPORT int luaopen_linenoise(lua_State *L)
 {
+    lua_pushliteral(L, "");
+    callback_error_ref = luaL_ref(L, LUA_REGISTRYINDEX);
+
     lua_newtable(L);
 
     luaL_newmetatable(L, LN_COMPLETION_TYPE);
